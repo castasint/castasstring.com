534b9c51d4a99ec15ae8360ee1526b2b8c1bc30a
castasint <phanuma@deloitte.com>
2021-05-14 12:29:23 +0530

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `castasstring.com`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Hanuma`,
+      summary: `who is Technology Architect building enterprise solutions`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `castasstring`,
     },
   },
   plugins: [
